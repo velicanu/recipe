e651a13d9df60a57e60eb583d3e74b5b0e593b91
Dragos Velicanu <dragosvelicanu@gmail.com>
2023-11-21 21:02:47 -0500

    fix test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,4 @@
+import os
 from tempfile import TemporaryDirectory
 
 from app import main
@@ -6,5 +7,6 @@ from app import main
 def test_main():
     expected = "done"
     with TemporaryDirectory() as temp_dir:
+        os.mkdir(os.path.join(temp_dir, "Nutrition"))
         actual = main(temp_dir)
     assert actual == expected
